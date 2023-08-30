982c0adc220a4fe7d930ba1851c23b9ad55f80d7
tiegai <tiegai842088@163.com>
2023-08-30 14:44:19 +0800

    add @interface 注释

--- src/main/java/org/example/annotation/Datas.java
+++ src/main/java/org/example/annotation/Datas.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Retention 表示生命周期
+ * Target 表示使用的目标范围
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD) //注释属性
 public @interface Datas {
--- src/main/java/org/example/annotation/IsUser.java
+++ src/main/java/org/example/annotation/IsUser.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * Retention 表示生命周期
+ * Target 表示使用的目标范围
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE) //注释类
 public @interface IsUser {
