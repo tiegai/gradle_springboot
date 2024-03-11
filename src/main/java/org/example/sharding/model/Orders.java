7a2a6ef4b3d7866f43be75c83650dd8dea439d4d
tiegai <tiegai842088@163.com>
2024-03-12 01:02:23 +0800

    @Date and @Builder

--- src/main/java/org/example/sharding/model/Orders.java
+++ src/main/java/org/example/sharding/model/Orders.java
@@ -1,50 +1,13 @@
 package org.example.sharding.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Orders {
     private Integer id;
     private Integer orderType;
     private Integer customerId;
     private Double amount;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getOrderType() {
-        return orderType;
-    }
-
-    public void setOrderType(Integer orderType) {
-        this.orderType = orderType;
-    }
-
-    public Integer getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Integer customerId) {
-        this.customerId = customerId;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    @Override
-    public String toString() {
-        return "Orders{" +
-                "id=" + id +
-                ", orderType=" + orderType +
-                ", customerId=" + customerId +
-                ", amount=" + amount +
-                '}';
-    }
 }
