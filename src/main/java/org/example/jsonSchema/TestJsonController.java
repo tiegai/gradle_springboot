eae802369ef58879fd18897869724212e75c62d4
chen2 <tiegai842088@163.com>
2024-06-02 02:47:56 +0800

    add jsonSchema

---
+++ src/main/java/org/example/jsonSchema/TestJsonController.java
@@ -0,0 +1,42 @@
+package org.example.jsonSchema;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/test")
+public class TestJsonController {
+
+    //TODO do test from postman1
+    @PostMapping("/user")
+    public String createUser(@RequestBody String json) throws ProcessingException, IOException {
+
+        JsonNode schemaNode = JsonLoader.fromResource("/twinSchema.json");
+
+        JsonSchema schema = JsonSchemaFactory.byDefault().getJsonSchema(schemaNode);
+
+        //解析用户提交的json数据
+        JsonNode userNode = JsonLoader.fromString(json);
+
+        //校验json是否符合规则
+        ProcessingReport validate = schema.validate(userNode);
+
+        if(validate.isSuccess()){
+            return "User is created successfully";
+        }else {
+            return validate.toString();
+        }
+
+    }
+
+}
