9a8526f45415446eb00de7c178c006ec29056ad4
chen2 <jerry.chen3@nike.com>
2024-06-02 00:43:33 +0800

    testgit

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -11,6 +11,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 public class MainApp {
+
+    //test git
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class,args);
     }
